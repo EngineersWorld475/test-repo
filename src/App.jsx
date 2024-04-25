0e63f1a8bba239b1b22a815724d67d7dd6f7b5f7
EngineersWorld457 <sanjaygnair777@gmail.com>
2024-04-24 18:30:39 -0700

    Added footer componant

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import Footer from './componants/Footer';
 
 function App() {
-  return <h1>My first Vite App</h1>;
+  return (
+    <>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
